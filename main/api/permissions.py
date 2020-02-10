56e6f8b60d2cba0d23860a1cae31951b94e88db0
JovanKroon <jovan@kroonstudio.com>
2020-02-10 15:48:33 +0100

    Permissions updated

--- main/api/permissions.py
+++ main/api/permissions.py
@@ -3,39 +3,40 @@ from rest_framework_role_filters.role_filters import RoleFilter
 from restfw_composed_permissions.base import (BaseComposedPermission, And, Or)
 from restfw_composed_permissions.generic.components import (AllowAll, AllowOnlyAuthenticated, AllowOnlySafeHttpMethod)
 from rest_condition import ConditionalPermission, C, And, Or, Not
+from rest_framework_role_filters.viewsets import RoleFilterModelViewSet
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     message = 'You must be the owner to edit or delete this object.'
 
     def has_object_permission(self, request, view, obj):
+        print(request.method)
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user.id
+        return obj.id == request.user.id
 
 class AdminPermission(permissions.BasePermission):
     message = 'You have to be admin to perform this action.'
-    safe_methods = ()
-
-    def has_permission(self, request, view):
-        pass
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
+        print(request.user_meta)
         for inst in request.user_meta['teams']:
-            if obj.team.id == inst['id'] and inst['role'] == 0:
+            if 19 == inst['id'] and inst['role'] == 0:
                 return True
         return False
 
-# class EditorPermission(permissions.BasePermission):
-#     message = 'You have to be editor to perform this action.'
-#     safe_methods = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
-#
-#     def has_permission(self, request, view):
-#         pass
-#
-#     def has_object_permission(self, request, view, obj):
-#         pass
+class EditorPermission(permissions.BasePermission):
+    message = 'You have to be editor to perform this action.'
+    safe_methods = ('GET', 'POST', 'PUT', 'PATCH')
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in self.safe_methods:
+            return True
+        for inst in request.user_meta['teams']:
+            if obj.team.id == inst['id'] and inst['role'] == 10:
+                return True
+        return False
 
 class ViewerPermission(permissions.BasePermission):
     message = 'You are restricted to viewer mode.'
--- users/api/views.py
+++ users/api/views.py
@@ -1,60 +1,21 @@
 from users.models import Users
 from rest_framework_simplejwt.views import TokenObtainPairView
 from users.api.serializers import UsersSerializer, MyTokenObtainPairSerializer
-from rest_framework.viewsets import ViewSet
-from rest_framework import status
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAdminUser
+from main.api.permissions import  *
 
-
-class UsersViewSet(ViewSet):
-    permission_classes_by_action = {'create': [AllowAny]}
-    def list(self, request):
-        queryset = Users.objects.all()
-        serializer = UsersSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Users.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UsersSerializer(user)
-        return Response(serializer.data)
-
-    def create(self, request):
-        data=request.data
-        serialized = UsersSerializer(data=data)
-        if serialized.is_valid():
-            serialized.save()
-            return Response(serialized.data, status=201)
-        else:
-            return Response(serialized.errors, status=201)
-
-    def update(self, request, pk=None):
-        inst = Users.objects.get(pk=pk)
-        data = request.data
-        serialized = UsersSerializer(instance=inst, data=data)
-        if serialized.is_valid():
-            serialized.save()
-            return Response(serialized.data, status=201)
-        else:
-            return Response(serialized.errors, status=201)
-
-    def partial_update(self, request, pk=None):
-        inst = Users.objects.get(pk=pk)
-        data = request.data
-        serialized = UsersSerializer(instance=inst, data=data, partial=True)
-        if serialized.is_valid():
-            serialized.save()
-            return Response(serialized.data, status=201)
-        else:
-            return Response(serialized.errors, status=201)
-
-    def destroy(self, request, pk=None):
-        users = Users.objects.get(pk=pk)
-        users.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UsersViewSet(ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    permission_classes_by_action = {
+        'create': [AllowAny],
+        'list': [IsAdminUser],
+        'retrieve': [AdminPermission],
+        'update': [IsOwnerOrReadOnly],
+        'partial_update': [IsOwnerOrReadOnly],
+        'destroy': [IsOwnerOrReadOnly]
+    }
 
     def get_permissions(self):
         try:
