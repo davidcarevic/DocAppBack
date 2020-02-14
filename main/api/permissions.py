31b24d7d95771b9ba9a3ad3c5018074fae757543
JovanKroon <jovan@kroonstudio.com>
2020-02-14 14:20:45 +0100

    Permissions

--- main/api/permissions.py
+++ main/api/permissions.py
@@ -4,7 +4,7 @@ from restfw_composed_permissions.base import (BaseComposedPermission, And, Or)
 from restfw_composed_permissions.generic.components import (AllowAll, AllowOnlyAuthenticated, AllowOnlySafeHttpMethod)
 from rest_condition import ConditionalPermission, C, And, Or, Not
 from rest_framework_role_filters.viewsets import RoleFilterModelViewSet
-from main.models import Roles
+from main.models import Roles, Teams
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     message = 'You must be the owner to edit or delete this object.'
@@ -19,30 +19,6 @@ class IsAdminOrOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.id == request.user.id or request.user.is_admin
-
-class BaseTeamLevelPermissions(permissions.BasePermission):
-    role = None
-    message = ''
-
-    def has_object_permission(self, request, view, obj):
-        try:
-            pass
-        except KeyError:
-            pass
-
-        return False
-
-class AdminProjectLevelPermissions(BaseTeamLevelPermissions):
-    message = "You need to be admin to perform this action!"
-    role = Roles.objects.get(name='admin')
-
-class EditorProjectLevelPermission(BaseTeamLevelPermissions):
-    message = "You need to have editor privileges to perform this action!"
-    role = Roles.objects.get(name='editor')
-
-class ViewerProjectLevelPermission(BaseTeamLevelPermissions):
-    message = "You are restricted to viewer mode!"
-    role = Roles.objects.get(name='viewer')
 
 class BaseProjectLevelPermission(permissions.BasePermission):
     role = None
